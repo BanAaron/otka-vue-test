54b0b131631a56b40aac2b678cbd7a2cd40d65d2
BanAaron <aarontbarratt@gmail.com>
2024-03-06 15:28:07 +0000

    OKTA working

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
+import sampleConfig from '@/config'
+import {OktaAuth} from '@okta/okta-auth-js'
+import OktaVue from '@okta/okta-vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const oktaAuth = new OktaAuth(sampleConfig.oidc)
+
+createApp(App)
+  .use(router)
+  .use(OktaVue, {oktaAuth})
+  .mount('#app')
